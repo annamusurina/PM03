fb8825785e5692babbad740b4836c25c0553fef1
Анна <stu-pksp118@AMDVLA3511GW06>
2022-04-19 09:52:03 +0300

    Работа с классом

--- src/src/src.cpp
+++ src/src/src.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 using namespace std;
 struct Route {
-	char number[10];
+	string number[10];
 	int ostanovki;
 	int time;
 	Route* next;
@@ -34,18 +34,64 @@ public:
 		else
 			return NULL;
 	}
-	int save(Route* mas, string name, int n) {// вводить данные в файл
+	int add(string n, double o, int t) {
+		Route* current, * previous, * tmp;
+		previous = NULL;
+		if (first) {
+			current = first;
+			do {
+				if (current->ostanovki > o && current->time > t) {
+					tmp = new Route;
+					tmp->number = n;
+					tmp->ostanovki = o;
+					tmp->time = t;					
+					tmp->next = current;
+					if (previous)
+						previous->next = tmp;
+					else
+						first = tmp;
+					break;
+				}
+				if (current->ostanovki == o && current->time == t)
+					return 0;
+				previous = current;
+				current = current->next;
+			} while (current);
+			if (!current) {
+				previous->next = new Route;
+				previous->next->number = n;
+				previous->next->ostanovki = o;
+				previous->next->time = t;
+				previous->next->next = NULL;
+			}
+		}
+		else {
+			Route* h = new Route;
+			h->number = n;
+			h->ostanovki = o;
+			h->time = t;
+			h->next = NULL;
+			first = h;
+		}
+		return 1;
+	}
+	int save(string name, int n) {// вводить данные в файл
 		ofstream fout(name);
 		if (fout)
 		{
-			for (int i = 0; i < n; i++) {
-				//Выводим все элементы структуры в файл
-				fout << mas[i].number << "  ";
-				fout << mas[i].ostanovki << "  ";
-				fout << mas[i].time << endl;
+			Route* current;
+			if (first) {
+				current = first;
+				for (int i = 0; i < n; i++) {
+					//Выводим все элементы структуры в файл
+					fout << current->number << "  ";
+					fout << current->ostanovki << "  ";
+					fout << current->time << endl;
+					current = current->next;
+				}
+				fout.close();
+				return 1;
 			}
-			fout.close();
-			return 1;
 		}
 		else {
 			cout << "Ошибка с файлом" << endl;
@@ -66,19 +112,23 @@ int main()
 	} while (N > 0);
 
 	TrolleybusStation* P = TrolleybusStation::getInstance();
-	Route* Routes = new Route[N];
+	
 	for (int i = 0; i < N; i++) {
+		string n;
+		int o, t;
 		cout << "Введите номер маршрута: ";
-		cin >> Routes[i].number[10];
+		cin >> n;
 		cout << "Введите количетсво остановок: ";
-		cin >> Routes[i].ostanovki;
+		cin >> o;
 		cout << "Введите время в пути в минутах: ";
-		cin >> Routes[i].time;
+		cin >> t;
+		P->add(n, o, t);
 	}
-
+	Route* Routes = P->one();
 	string fl;
 	cout << "Введите имя файла: ";
 	cin >> fl;
+	P->save(fl, N);
 	
 
 	system("pause");
